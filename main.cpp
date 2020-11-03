d8f562476de55c3a42389702d2f20890cc8f1547
Alex <kontakt@alexander-regier.de>
2020-11-03 16:19:56 +0100

    implement ui concept

--- main.cpp
+++ main.cpp
@@ -13,6 +13,11 @@
 #include <stdio.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
+#define FLAGS_BASE (0 | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar)
+#define FLAGS_EDITOR (FLAGS_BASE)
+#define FLAGS_HELP (FLAGS_BASE)
+#define FLAGS_CANVAS (FLAGS_BASE)
+#define FLAGS_CONSOLE (FLAGS_BASE)
 
 // Main code
 int main(int, char**)
@@ -46,8 +51,8 @@ int main(int, char**)
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
+    //ImGui::StyleColorsDark();
+    ImGui::StyleColorsClassic();
 
     // Setup Platform/Renderer backends
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
@@ -69,7 +74,10 @@ int main(int, char**)
     //IM_ASSERT(font != NULL);
 
     // Our state
-    bool show_another_window = false;
+   // bool show_app_console = true;
+    bool show_demo_window = true;
+    
+    
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop
@@ -99,43 +107,172 @@ int main(int, char**)
        
 
         // 1. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        
+        ImGui::ShowDemoWindow(&show_demo_window); 
+        
+        if (ImGui::BeginMainMenuBar())
         {
 
-            static int counter = 0;
+            ImGui::Text("HELP - Hilfe anzeigen ");
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::Text("CRTL + C - Programm beenden ");
 
-            ImGui::Text("Here you can open a second window!");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Another Window", &show_another_window);
+            ImGui::Text("RUN - Programm starten ");
 
 
-            if (ImGui::Button("Increment"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-                
-            if (ImGui::Button("Reset"))
-                counter = 0;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-            
-            
-
-            ImGui::End();
+            ImGui::EndMainMenuBar();
         }
+            
+        ImGui::SetNextWindowPos(ImVec2(60, 50), ImGuiCond_None);
+        ImGui::SetNextWindowSize(ImVec2(700, 950), ImGuiCond_None);
+    
+        
+        ImGui::Begin("Editor (F1 - Taste)", NULL, FLAGS_EDITOR);                          // Create a window called "Hello, world!" and append into it.
+            
+            
+            
+            static char text[1024 * 16] = 
+            "10 PRINT HELLO WORLD\n"
+            "20 START\n"
+            "30 RESET";
+            static ImGuiInputTextFlags flags = ImGuiInputTextFlags_AllowTabInput;
+            ImGui::InputTextMultiline("##source", text, IM_ARRAYSIZE(text), ImVec2(-FLT_MIN, 915), flags);
+        
+        
+        ImGui::End();
+        
+             
+        ImGui::SetNextWindowPos(ImVec2(820, 50), ImGuiCond_None);
+        ImGui::SetNextWindowSize(ImVec2(500, 500), ImGuiCond_None);
+        
+        //Help window
+        ImGui::Begin("Help Window (F3 - Taste)", NULL, FLAGS_HELP);
+        static char buf[32] = "\xe6\x97\xa5\xe6\x9c\xac\xe8\xaa\x9e"; 
+        ImGui::InputText("Search", buf, IM_ARRAYSIZE(buf));
+        ImGui::TextWrapped(
+        "\nThis text is just a Test\n"
+        "This text is just a Test\n"
+        "This text is just a Test\n"
+        "This text is just a Test\n"
+        "This text is just a Test\n"
+        "This text is just a Test\n"
+        "This text is just a Test\n"
+        "This text is just a Test\n"
+        "This text is just a Test\n"
+        "This text is just a Test\n"
+        );
+        ImGui::End();
 
-        // 2. Show another simple window.
-        if (show_another_window)
+        // 2. Show another simple window.s
+        
+        ImGui::SetNextWindowPos(ImVec2(1350, 50), ImGuiCond_None);
+        ImGui::SetNextWindowSize(ImVec2(500, 500), ImGuiCond_None);
+    
+        ImGui::Begin("Graphical Output (F4 - Taste)", NULL, FLAGS_CANVAS);   
+        
+        
+        static ImVector<ImVec2> points;
+        static ImVec2 scrolling(0.0f, 0.0f);
+        static bool opt_enable_grid = true;
+        static bool opt_enable_context_menu = true;
+        static bool adding_line = false;
+        
+        ImVec2 canvas_p0 = ImGui::GetCursorScreenPos(); // ImDrawList API uses screen coordinates!
+        ImVec2 canvas_sz = ImGui::GetContentRegionAvail(); // Resize canvas to what's available
+        if (canvas_sz.x < 50.0f) canvas_sz.x = 50.0f;
+        if (canvas_sz.y < 50.0f) canvas_sz.y = 50.0f;
+        ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
+        
+        // Draw border and background color
+        ImGuiIO& io = ImGui::GetIO();
+        ImDrawList* draw_list = ImGui::GetWindowDrawList();
+        draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
+        draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
+        
+        // This will catch our interactions
+        ImGui::InvisibleButton("canvas", canvas_sz, ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
+        const bool is_hovered = ImGui::IsItemHovered();
+        const bool is_active = ImGui::IsItemActive();
+        const ImVec2 origin(canvas_p0.x + scrolling.x, canvas_p0.y + scrolling.y);
+        const ImVec2 mouse_pos_in_canvas(io.MousePos.x- origin.x, io.MousePos.y - origin.y);
+        
+        // Add first and second point
+        if(is_hovered && !adding_line && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
         {
-            ImGui::Begin("Window 2", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
+            points.push_back(mouse_pos_in_canvas);
+            points.push_back(mouse_pos_in_canvas);
+            adding_line = true;
         }
+        if(adding_line)
+        {
+            points.back() = mouse_pos_in_canvas;
+            if(!ImGui::IsMouseDown(ImGuiMouseButton_Left))
+                adding_line = false;
+        }
+        
+        // Pan (we use a zero mouse threshold when there's no context menu)
+        // You may decide to make that threshold dynmaic based on wether th mouse is hovering something etc.
+        const float mouse_threshold_for_pan =opt_enable_context_menu ? -1.0f : 0.0f;
+        if (is_active && ImGui::IsMouseDragging(ImGuiMouseButton_Right, mouse_threshold_for_pan))
+        {
+            scrolling.x += io.MouseDelta.x;
+            scrolling.y += io.MouseDelta.y;
+        }
+        
+        // Context menu (under default mouse threshold)
+        ImVec2 drag_delta = ImGui::GetMouseDragDelta(ImGuiMouseButton_Right);
+        if (opt_enable_context_menu && ImGui::IsMouseReleased(ImGuiMouseButton_Right) && drag_delta.x == 0.0f && drag_delta.y == 0.0f)
+            ImGui::OpenPopupOnItemClick("context");
+        if (ImGui::BeginPopup("context"))
+        {
+            if (adding_line) 
+                points.resize(points.size() - 2);
+            adding_line = false;
+            if (ImGui::MenuItem("Remove one", NULL, false, points.Size > 0)) { points.resize(points.size() - 2); }
+            if (ImGui::MenuItem("Remove all", NULL, false, points.Size > 0)) { points.clear(); }  
+            ImGui::EndPopup();
+        }
+        
+        // Draw grid + all lines in the canvas
+        draw_list->PushClipRect(canvas_p0, canvas_p1, true);
+        if (opt_enable_grid)
+        {
+            const float GRID_STEP = 16.0f;
+            for (float x = fmodf(scrolling.x, GRID_STEP); x < canvas_sz.x; x += GRID_STEP)
+                draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y), ImVec2(canvas_p0.x + x, canvas_p1.y), IM_COL32(200, 200, 200, 40));
+            for (float y = fmodf(scrolling.y, GRID_STEP); y < canvas_sz.y; y += GRID_STEP)
+                draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y), IM_COL32(200, 200, 200, 40));
+        }
+        for (int n = 0; n < points.Size; n += 2)
+            draw_list->AddLine(ImVec2(origin.x + points[n].x, origin.y + points[n].y), ImVec2(origin.x + points[n + 1].x, origin.y + points[n + 1].y), IM_COL32(255, 255, 0, 255), 2.0f);
+        draw_list->PopClipRect();
+        
+        
+        
+        
+        /*ImGuiIO& io = ImGui::GetIO();
+        ImTextureID my_tex_id = io.Fonts->TexID;
+        float my_tex_w = (float)io.Fonts->TexWidth;
+        float my_tex_h = (float)io.Fonts->TexHeight;
+        {
+        ImGui::Text("%.0fx%.0f", my_tex_w, my_tex_h);
+        //ImVec2 pos = ImGui::GetCursorScreenPos();
+        ImVec2 uv_min = ImVec2(0.0f, 0.0f); // Top-Left
+        ImVec2 uv_max = ImVec2(1.0f, 1.0f); // lower right
+        ImVec4 tint_col = ImVec4(1.0f, 1.0f, 1.0f, 1.0f); // No tint
+        ImVec4 border_col = ImVec4(1.0f, 1.0f, 1.0f, 0.5f); // 50% opaque white      
+        ImGui::Image(my_tex_id, ImVec2(my_tex_w, my_tex_h), uv_min, uv_max, tint_col, border_col);
+        } */  
+        ImGui::End();
+        
         
         // 3. Show window number 3.
         
-        ImGui::Begin("Zwei Fenster sind langweilig");
-            ImGui::Text("I'm window number 3!");
+        ImGui::SetNextWindowPos(ImVec2(820, 570), ImGuiCond_None);
+        ImGui::SetNextWindowSize(ImVec2(1030, 430), ImGuiCond_None);
+        
+        ImGui::Begin("Console (F2 - Taste)", NULL, FLAGS_CONSOLE);
+          
         
         ImGui::End();
 
