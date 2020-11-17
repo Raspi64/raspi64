d6c6e6ad77fd86932674b9b273d49763cd79a9e9
Alexander Regier <kontakt@alexander-regier.de>
2020-11-17 16:36:35 +0100

    enable keyboard navigation

--- src/Gui.cpp
+++ src/Gui.cpp
@@ -83,7 +83,7 @@ void Gui::init_imgui() {
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
     (void) io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
