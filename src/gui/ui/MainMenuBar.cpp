b6edb16ea876152ee02e8f89fe5ddd1802133725
Alexander Regier <kontakt@alexander-regier.de>
2020-12-08 15:04:50 +0100

    show current language in main menu bar

--- src/gui/ui/MainMenuBar.cpp
+++ src/gui/ui/MainMenuBar.cpp
@@ -5,11 +5,16 @@
 #include "imgui.h"
 #include "MainMenuBar.hpp"
 
+MainMenuBar::MainMenuBar(LANG *currentLanguage) : current_language(currentLanguage) {}
+
 void MainMenuBar::render() {
     if (ImGui::BeginMainMenuBar()) {
         ImGui::Text("HELP - Hilfe anzeigen ");
         ImGui::Text("CRTL + C - Programm beenden ");
         ImGui::Text("RUN - Programm starten ");
+
+        ImGui::Text("Aktuelle Sprache:");
+        ImGui::Text((*current_language) == LUA ? "Lua" : "Basic");
 
         ImGui::Text("                 [ F12 ] Herunterfahren");
 
--- src/gui/ui/MainMenuBar.hpp
+++ src/gui/ui/MainMenuBar.hpp
@@ -5,11 +5,17 @@
 #ifndef IMGUI_SETUP_MAINMENUBAR_HPP
 #define IMGUI_SETUP_MAINMENUBAR_HPP
 
+#include <types.hpp>
 #include "UiElement.hpp"
 
 class MainMenuBar: public UiElement {
 public:
+    MainMenuBar(LANG* current_language);
+
     void render();
+
+private:
+    LANG* current_language;
 };
 
 #endif //IMGUI_SETUP_MAINMENUBAR_HPP
