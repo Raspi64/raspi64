518d04c5cb7a12914675cc27e64b2f1efff6f3e2
Simon Struck <simon@simonscode.org>
2021-01-02 13:03:50 +0100

    Update buildscript

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
-CMAKE_BUILD_DIR="./cmake-build-debug/"
+CMAKE_BUILD_DIR="./cmake-build/"
+
+if test -f /proc/cpuinfo; then
+  NUM_CPUS="$(grep siblings /proc/cpuinfo | head -n1 | cut -d':' -f2 | cut -d' ' -f2)"
+else
+  NUM_CPUS="$(sysctl -n hw.ncpu)"
+fi
 
 mkdir -p "$CMAKE_BUILD_DIR"
 
 cd "$CMAKE_BUILD_DIR" || exit 1
 cmake .. || exit 1
-make || exit 1
+make -j "$NUM_CPUS" || exit 1
