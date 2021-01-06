78de07af9b5755e94a770a27d97ff06dc96cc343
Alexander Regier <kontakt@alexander-regier.de>
2021-01-06 09:24:21 +0100

    forward in window handler

--- src/gui/ui/window/GraphicWindowHandler.cpp
+++ src/gui/ui/window/GraphicWindowHandler.cpp
@@ -8,6 +8,25 @@
 
 GraphicWindowHandler::GraphicWindowHandler(GraphicWindow *window): WindowHandler(window), window(window) {}
 
+void GraphicWindowHandler::add_rect(TGraphicRect rect) {
+    window->add_rect(rect);
+}
+
+void GraphicWindowHandler::add_circle(TGraphicCircle circle) {
+    window->add_circle(circle);
+}
+
+void GraphicWindowHandler::add_line(TGraphicLine line) {
+    window->add_line(line);
+}
+
+void GraphicWindowHandler::add_text(TGraphicText text) {
+    window->add_text(text);
+}
+
+void GraphicWindowHandler::add_pixel(TGraphicPixel pixel) {
+    window->add_pixel(pixel);
+}
 void GraphicWindowHandler::add_pixel(int x, int y, int red, int green, int blue, int alpha, int size) {
     window->add_pixel(x, y, red, green, blue, alpha, size);
 }
--- src/gui/ui/window/GraphicWindowHandler.hpp
+++ src/gui/ui/window/GraphicWindowHandler.hpp
@@ -14,6 +14,11 @@
 class GraphicWindowHandler: public WindowHandler {
 public:
     GraphicWindowHandler(GraphicWindow* window);
+    void add_pixel(TGraphicPixel pixel);
+    void add_rect(TGraphicRect rect);
+    void add_circle(TGraphicCircle circle);
+    void add_line(TGraphicLine line);
+    void add_text(TGraphicText text);
     void add_pixel(int x, int y, int red, int green, int blue, int alpha = 255, int size = 1);
     void clear_pixels();
 private:
