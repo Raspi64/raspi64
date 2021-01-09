94d365193144ddeecff16453a34c4a3e54aa2442
Simon Struck <simon@simonscode.org>
2021-01-09 02:58:10 +0100

    Change example start

---
+++ saves/example_start.lua
@@ -0,0 +1,27 @@
+local x = 250
+local y = 250
+local r = 4
+local d_r = 10
+local t = 0.0001
+
+clear()
+
+while true do
+	for i=1,45 do
+		for angle=0, 360, 1 do
+			d_x = r * math.cos(angle * math.pi / 180);
+			d_y = r * math.sin(angle * math.pi / 180);
+			local c_r = 255 * angle / 360
+			local c_g = 255 - (255 * angle / 360)
+			local c_b = (120 * (angle / 360) + 127) % 255
+			local c_b = 255 - ((x+d_x+y + d_y) / 1000 * 255)
+			-- print(x + d_x, y + d_y, c_r, c_g, c_b)
+			draw(math.floor(x + d_x), math.floor(y + d_y), math.floor(c_r), math.floor(c_g), math.floor(c_b), 255, 5);
+			sleep(t)
+			r = r + d_r / 500
+		end
+	end
+	clear()
+	r = 4
+end
+
